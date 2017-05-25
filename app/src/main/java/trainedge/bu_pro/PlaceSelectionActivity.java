52f64525b2c68394ff685df5ca4eb51c692ca070
zaid bin kamil <xaidmetamorphos@gmail.com>
2017-05-25 19:45:03 +0530

    Updated app with the map

---
+++ app/src/main/java/trainedge/bu_pro/PlaceSelectionActivity.java
@@ -0,0 +1,98 @@
+package trainedge.bu_pro;
+
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class PlaceSelectionActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    public static final int REQUEST_MAP_PERMISSION = 333;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_selection);
+        handlePermission();
+
+    }
+
+    private void handlePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED || checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
+                }, REQUEST_MAP_PERMISSION);
+                return;
+
+            }else{
+                loadMap();
+            }
+        }
+        else
+        {
+            loadMap();
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode== REQUEST_MAP_PERMISSION){
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    handlePermission();
+
+                }
+            }
+            loadMap();
+        }
+    }
+
+    private void loadMap() {
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        CircleOptions co = new CircleOptions();
+        co.center(sydney);
+        co.radius(200);
+        co.fillColor(Color.argb(55,0,0,0));
+        mMap.addCircle(co);
+
+    }
+}
--- app/src/main/java/trainedge/bu_pro/ProfileCreationActivity.java
+++ app/src/main/java/trainedge/bu_pro/ProfileCreationActivity.java
@@ -1,13 +1,29 @@
 package trainedge.bu_pro;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class ProfileCreationActivity extends AppCompatActivity {
+public class ProfileCreationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int REQUEST_ADDRESS = 22;
+    private FloatingActionButton fabChoose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_creation);
+        fabChoose = (FloatingActionButton) findViewById(R.id.fabChoose);
+        fabChoose.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this,PlaceSelectionActivity.class);
+        startActivityForResult(i, REQUEST_ADDRESS);
+
     }
 }
